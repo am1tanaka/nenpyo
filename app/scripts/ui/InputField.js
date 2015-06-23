4c75e5226ffdff129e3097800e8f7a7467a0448d
YuTanaka <YRK00337@nifty.com>
2015-06-24 02:55:44 +0900

    typeaheadを組み込んでオートコンプリートの実装開始。サンプルで動作を確認。

--- app/scripts/ui/InputField.js
+++ app/scripts/ui/InputField.js
@@ -9,6 +9,17 @@ var React = require('react/addons'),
     Variables = require('../variables'),
     YearConverter = require('../YearConverter');
 
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+
 var MenuYearType = React.createClass({
   render: function()
   {
@@ -36,6 +47,26 @@ var InputField = React.createClass({
       tags: ""
     };
   },
+  // suggestion engine
+  states : new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    // `states` is an array of state names defined in "The Basics"
+    local: states
+  }),
+  componentDidMount: function() {
+    React.findDOMNode(this.refs.inputTag).focus();
+    $ (React.findDOMNode(this.refs.inputTag)).typeahead({
+      hint: true,
+      highlight: true,
+      minLength: 1
+    },
+    {
+      name: 'states',
+      source: this.states
+    });
+  },
+
   // 西暦、和暦をドロップダウンから選択した時の処理
   handleChangeYearType: function(e) {
     this.setState({yearType: e.currentTarget.textContent});
@@ -171,9 +202,12 @@ var InputField = React.createClass({
                   タグ
                 </label>
                 <div className="col-sm-11">
-                  <input type="text" id="textTag" className="form-control" placeholder="タグ(省略可)"
-                    maxLength={Variables.TAG_MAX}
-                    value={this.state.tags} />
+                  <div className="bloodhound">
+                    <input type="text" id="textTag" className="typeahead" placeholder="タグ(省略可)"
+                      maxLength={Variables.TAG_MAX}
+                      ref="inputTag" name="inputTag"
+                      />
+                  </div>
                   <p className='help-block'>複数指定する場合は、スペースで区切ってください。</p>
                 </div>
               </div>
@@ -208,4 +242,3 @@ var InputField = React.createClass({
 
 
 module.exports = InputField;
-
