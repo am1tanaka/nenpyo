393292629dcbb2cbab31bea790e5c40bab8d2637
YuTanaka <YRK00337@nifty.com>
2015-06-25 19:37:29 +0900

    不要になったデータを削除

--- app/scripts/ui/InputField.js
+++ app/scripts/ui/InputField.js
@@ -9,17 +9,6 @@ var React = require('react/addons'),
     Variables = require('../variables'),
     YearConverter = require('../YearConverter');
 
-var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
-
 var MenuYearType = React.createClass({
   render: function()
   {
