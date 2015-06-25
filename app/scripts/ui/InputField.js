e0e3df86e03c11fa2ee4e124f5e4aeba65939867
YuTanaka <YRK00337@nifty.com>
2015-06-26 00:34:46 +0900

    タグの追加、削除操作を実装

--- app/scripts/ui/InputField.js
+++ app/scripts/ui/InputField.js
@@ -17,19 +17,37 @@ var MenuYearType = React.createClass({
   }
 });
 
+var PanelHead = React.createClass({
+  render: function() {
+    var handleClose = this.props.onClick;
+    return (
+      <div className="panel-heading">
+        <div className="panel-title">
+          年表の新規入力&nbsp;&nbsp;
+          <button id="btnCloseInputField" className="btn btn-default"
+            title="入力欄を閉じる" data-placement="bottom" data-toggle="tooltip"
+            type="button" onClick={handleClose}>
+              <span className="glyphicon glyphicon-remove" />閉じる
+          </button>
+        </div>
+      </div>
+    );
+  }
+});
+
 /**
  * this.props.tags配列で渡されたタグの文字列を、ボタン形式で、
  * 削除つきのブロックとして返す
  */
 var ButtonTagList = React.createClass({
   render: function() {
+    var handleRemove = this.props.handleRemove;
     var ret = this.props.tags.map(function(tag) {
       return (
         <span key={'tagkouho_'+tag} className='framed-text'>{tag}&nbsp;
           <span className='btn' key={tag}
             onClick={function(e){
-              var deltag = tag;
-              alert(deltag);
+              handleRemove(tag);
             }}>&times;</span>
         </span>
       );
@@ -59,6 +77,24 @@ var InputField = React.createClass({
       nowSelectedTags: []
     };
   },
+  // 西暦と和暦を変換したものを表示する
+  getConvYear: function() {
+    var mon = 0;
+    var day = 0;
+    if (this.state.year.length == 0) {
+      return "-";
+    }
+
+    if (this.state.yearType == "西暦") {
+      // 西暦＞和暦変換
+      mon = this.state.month.length == 0 ? 12 : this.state.month;
+      day = this.state.day.length == 0 ? 31 : this.state.day;
+      return YearConverter.convYear(this.state.year,mon,day);
+    }
+
+    // 和暦＞西暦変換
+    return YearConverter.convWareki2AD(this.state.yearType, this.state.year);
+  },
   // 西暦、和暦をドロップダウンから選択した時の処理
   handleChangeYearType: function(e) {
     this.setState({yearType: e.currentTarget.textContent});
@@ -87,59 +123,81 @@ var InputField = React.createClass({
       this.setState({day: newval});
     }
   },
-  // enterキーのキーアップを検出
-  handleKeyupTag: function(e) {
-    if (e.keyCode == 13) {
-      var txttag = $('#textTag');
-      var intag = txttag.val();
-      if (intag.length > 0) {
-        var newar = this.state.nowSelectedTags;
-        newar.push(intag);
-        this.setState({nowSelectedTags:newar});
-        //
-        txttag.val('');
-        //$ (React.findDOMNode(this.refs.inputTag)).empty();
-      }
+  // タグ追加ボタン
+  handleAddTag: function(e) {
+    var txttag = $('#textTag');
+    var intag = txttag.val();
+    if (intag.length > 0) {
+      var newar = this.state.nowSelectedTags;
+      newar.push(intag);
+      // 選択中のタグを更新する
+      this.setState({nowSelectedTags:newar});
+
+      // 入力欄をクリアする
+      txttag.typeahead('val','');
+
+      // 既存の選択肢リストから、選択済みの選択肢を外した配列を作成する
+      this.refreshBloodhoundTag();
+        txttag.focus().typeahead('open');
     }
   },
-  // 西暦と和暦を変換したものを表示する
-  getConvYear: function() {
-    var mon = 0;
-    var day = 0;
-    if (this.state.year.length == 0) {
-      return "-";
-    }
+  refreshBloodhoundTag: function() {
+    var nowsel = this.state.nowSelectedTags;
+    var newlist = this.props.tagList.filter(function(tag) {
+        return ($.inArray(tag,nowsel) === -1);
+    });
 
-    if (this.state.yearType == "西暦") {
-      // 西暦＞和暦変換
-      mon = this.state.month.length == 0 ? 12 : this.state.month;
-      day = this.state.day.length == 0 ? 31 : this.state.day;
-      return YearConverter.convYear(this.state.year,mon,day);
-    }
-
-    // 和暦＞西暦変換
-    return YearConverter.convWareki2AD(this.state.yearType, this.state.year);
+    var txttag = $('#textTag');
+    // 選択肢を再設定
+    this.bloodhoundTag.clear();
+    this.bloodhoundTag.local = newlist;
+    this.bloodhoundTag.initialize(true);
+    txttag.typeahead('destroy');
+    txttag.typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 0
+      },
+      {
+        name: 'tags',
+        source: this.tagsWithDefaults
+      });
   },
+  handleRemoveNowSelectedTag: function(tag) {
+    var newar = this.state.nowSelectedTags;
+    var idx = $.inArray(tag,newar);
+    if (idx !== -1)
+    {
+      newar.splice(idx,1);
+      // タグリストを更新する
+      this.refreshBloodhoundTag(newar);
+      this.setState({nowSelectedTags: newar});
+    }
+  },
+  bloodhoundTag: null,
   tagsWithDefaults(q, sync) {
+    if (q === '') {
+      // 空欄の時は全てを返す
+      sync(this.bloodhoundTag.all());
+    }
+    else {
+      // 一致するリストを返す
+      this.bloodhoundTag.search(q, sync);
+    }
+  },
+  componentDidMount: function() {
     // suggestion engine
-    var tags = new Bloodhound({
+    this.bloodhoundTag = new Bloodhound({
       datumTokenizer: Bloodhound.tokenizers.whitespace,
       queryTokenizer: Bloodhound.tokenizers.whitespace,
       // an array of tags defined in "The Basics"
       local: this.props.tagList
     });
 
-    if (q === '') {
-      // 空欄の時は全てを返す
-      sync(this.props.tagList);
-    }
-    else {
-      tags.search(q, sync);
-    }
-  },
-  componentDidMount: function() {
-    React.findDOMNode(this.refs.inputTag).focus();
-    $ (React.findDOMNode(this.refs.inputTag)).typeahead({
+    this.bloodhoundTag.initialize();
+
+    // set typeahead
+    $ ('#textTag').typeahead({
       hint: true,
       highlight: true,
       minLength: 0
@@ -148,7 +206,6 @@ var InputField = React.createClass({
       name: 'tags',
       source: this.tagsWithDefaults
     });
-
   },
   render: function() {
     // 和暦と西暦を確認のために変換する
@@ -162,16 +219,7 @@ var InputField = React.createClass({
           id='confirmEntry' />
 
         <div className="panel panel-default">
-          <div className="panel-heading">
-            <div className="panel-title">
-              年表の新規入力&nbsp;&nbsp;
-              <button id="btnCloseInputField" className="btn btn-default"
-                title="入力欄を閉じる" data-placement="bottom" data-toggle="tooltip"
-                type="button" onClick={this.props.handleCloseInput}>
-                  <span className="glyphicon glyphicon-remove" />閉じる
-              </button>
-            </div>
-          </div>
+          <PanelHead onClick={this.props.handleCloseInput} />
           <div className="panel-body">
             <form className="form-horizontal">
               <div className="form-group">
@@ -235,16 +283,25 @@ var InputField = React.createClass({
                   タグ
                 </label>
                 <div className="col-sm-11">
-                  <ButtonTagList tags={this.state.nowSelectedTags} />
-                  <div className="bloodhound">
-                    <input type="text" id="textTag" className="typeahead form-control" placeholder="タグ(省略可)"
-                      size={Variables.TAG_MAX}
-                      maxLength={Variables.TAG_MAX}
-                      ref="inputTag" name="inputTag"
-                      onKeyUp={this.handleKeyupTag}
-                      />
+                  <ButtonTagList tags={this.state.nowSelectedTags} handleRemove={this.handleRemoveNowSelectedTag} />
+                  <div className='form-inline'>
+                    <div className="bloodhound">
+                      <input type="text"
+                        id="textTag"
+                        name="inputTag"
+                        className="typeahead form-control"
+                        placeholder="タグ(省略可)"
+                        maxLength={Variables.TAG_MAX}
+                        size={Variables.TAG_MAX*2}
+                        />
+                    </div>
+                    <button type='button'
+                      className='btn btn-default form-control'
+                      onClick={this.handleAddTag}
+                      >
+                      <span className="glyphicon glyphicon-plus" />&nbsp;タグ追加
+                    </button>
                   </div>
-                  <p className='help-block'>タグを選んで[Enter]を押すと複数のタグを指定できます。</p>
                 </div>
               </div>
 
@@ -252,9 +309,13 @@ var InputField = React.createClass({
                 <div className="col-sm-offset-1 col-sm-11">
 
                   <div className="col-sm-2">
-                    <button type="button" className="btn btn-primary form-control"
-                      data-toggle="modal" data-target="#confirmEntry"
-                      title="入力した年表を登録する" data-placement="bottom">
+                    <button type="button"
+                      className="btn btn-primary form-control"
+                      data-toggle="modal"
+                      data-target="#confirmEntry"
+                      title="入力した年表を登録する"
+                      data-placement="bottom"
+                      >
                       <span className="glyphicon glyphicon-plus" />&nbsp;登録
                     </button>
                   </div>
